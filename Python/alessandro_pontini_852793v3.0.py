491b052b66592c836ecb715c666ab8671b8b0bbe
alessandro_pontini <a.pontini1@campus.unimib.it>
2020-02-20 09:57:44 +0100

    add v  final python project

--- Python/alessandro_pontini_852793v3.0.py
+++ Python/alessandro_pontini_852793v3.0.py
@@ -5,7 +5,7 @@
 
 # ***
 
-# In[71]:
+# In[1]:
 
 
 # importo librerie
@@ -15,14 +15,14 @@ import numpy as np
 pd.options.mode.chained_assignment = None
 
 
-# In[10]:
+# In[2]:
 
 
 # scrivo directory
-dire = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/loans_lenders.csv'
-dire1 = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/loans.csv'
-dire2 = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/lenders.csv'
-dire3 = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/country_stats.csv'
+dire_df_lenders_not_normalized = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/loans_lenders.csv'
+dire_df_loans = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/loans.csv'
+dire_df_lenders_info = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/lenders.csv'
+dire_df_country_stat = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-snapshot/country_stats.csv'
 
 
 # ***
@@ -31,20 +31,20 @@ dire3 = '/Users/alessandropontini/Documents/db/progetto_python/additional-kiva-s
 
 # ***
 
-# In[11]:
+# In[3]:
 
 
 # leggo il file csv
-df_lenders_not_normalized = pd.read_csv(dire)
+df_lenders_not_normalized = pd.read_csv(dire_df_lenders_not_normalized)
 
 
-# In[12]:
+# In[4]:
 
 
 df_lenders_not_normalized.head()
 
 
-# In[13]:
+# In[5]:
 
 
 # impongo index=loan_id per poter usare explode che per ogni loan_id mi prende ogni elemento della lista creata con la strip 
@@ -52,7 +52,7 @@ df_lenders_not_normalized.head()
 df_lenders_normalized = df_lenders_not_normalized.set_index('loan_id')['lenders'].str.split(pat=',').explode().reset_index()
 
 
-# In[14]:
+# In[6]:
 
 
 # mostro risultato
@@ -65,15 +65,15 @@ df_lenders_normalized.head()
 
 # ***
 
-# In[15]:
+# In[7]:
 
 
 # leggo il file loans, ma per la quantità di dati che devo leggere
 # preferisco leggere le date come tali e non come stringhe.
-df_loans = pd.read_csv(dire1, parse_dates=['disburse_time', 'planned_expiration_time'])
+df_loans = pd.read_csv(dire_df_loans, parse_dates=['disburse_time', 'planned_expiration_time'])
 
 
-# In[32]:
+# In[8]:
 
 
 # ci sono delle date che non hanno disburse_time > planned_expiration_time, non ha senso un prestito che si estingue
@@ -82,13 +82,13 @@ df_loans = pd.read_csv(dire1, parse_dates=['disburse_time', 'planned_expiration_
 temp_df = df_loans.loc[df_loans['disburse_time'] > df_loans['planned_expiration_time']]
 
 
-# In[33]:
+# In[9]:
 
 
 temp_df[['disburse_time', 'planned_expiration_time']]
 
 
-# In[34]:
+# In[10]:
 
 
 # inverto i valori
@@ -100,27 +100,27 @@ temp_df.planned_expiration_time = temp_df.temp_date
 del temp_df['temp_date']
 
 
-# In[35]:
+# In[11]:
 
 
 temp_df[['disburse_time', 'planned_expiration_time']]
 
 
-# In[36]:
+# In[12]:
 
 
 # uso funzione isin per vedere i loan_id che coincidano e cambio df_loan con i nuovi di df_temp corretti
 df_loans.loc[df_loans.loan_id.isin(temp_df.loan_id), ['disburse_time', 'planned_expiration_time']] = temp_df[['disburse_time','planned_expiration_time']]
 
 
-# In[37]:
+# In[13]:
 
 
 # sono stati invertiti
 df_loans.loc[df_loans['disburse_time'] > df_loans['planned_expiration_time']]
 
 
-# In[38]:
+# In[14]:
 
 
 # creo una colonna per la duration e faccio differenza, dopo di che mostro il risultato
@@ -134,20 +134,23 @@ df_loans[['loan_id','duration']]
 
 # ***
 
-# In[107]:
+# In[15]:
 
 
 temp_df = df_loans[['loan_id', 'status', 'loan_amount']]
 
 
-# In[108]:
+# In[16]:
 
 
 # trovo df che mi serve per punto 3 in temp_df
 # trovo df2 che mi da' il totale del lent per i punti successivi
 temp_df2 = df_lenders_not_normalized.merge(temp_df, how='left', left_on='loan_id', right_on='loan_id')
+# ci sono 4 valori null
+temp_df2 = temp_df2.fillna(0)
 total_lent = temp_df2.loan_amount.sum()
 temp_df = df_lenders_normalized.merge(temp_df, how='left', left_on='loan_id', right_on='loan_id')
+
 
 temp_df = temp_df.loc[temp_df.status == 'funded']
 temp_df = temp_df.groupby('lenders').size().reset_index(name='count').sort_values(by='count', ascending=False)
@@ -162,7 +165,7 @@ temp_df.loc[temp_df['count'] >= 2]
 
 # ***
 
-# In[109]:
+# In[17]:
 
 
 # suppongo che in loans che country_code identifichi le nazionalità delle persone perciò con una groupby posso raggrupparle
@@ -176,7 +179,7 @@ df_loans.groupby('country_code').size()
 
 # ***
 
-# In[110]:
+# In[18]:
 
 
 # raggruppo in base allo stato di appartenenza scegliendo di mostrare la colonna loan_amount e sommo i risultati.
@@ -189,7 +192,7 @@ df_loans.groupby('country_code')['loan_amount'].sum()
 
 # ***
 
-# In[111]:
+# In[19]:
 
 
 # uso il total_lent calcolato in precedenza
@@ -202,28 +205,28 @@ df_loans.groupby('country_code')['loan_amount'].sum()/total_lent*100
 
 # ***
 
-# In[116]:
+# In[20]:
 
 
 temp_df = df_loans[['disburse_time', 'country_code', 'loan_amount']]
 temp_df.disburse_time = temp_df['disburse_time'].dt.tz_localize(None).dt.year
 
 
-# In[119]:
+# In[21]:
 
 
 # vedo quante persone all'anno punto 4
 temp_df.groupby(['country_code','disburse_time']).size()
 
 
-# In[120]:
+# In[22]:
 
 
 # faccio le somme punto 5
 temp_df.groupby(['country_code','disburse_time'])['loan_amount'].sum()
 
 
-# In[122]:
+# In[23]:
 
 
 # uso total_lent precedente
@@ -236,7 +239,7 @@ temp_df.groupby(['country_code', 'disburse_time'])['loan_amount'].sum()/total_le
 
 # ***
 
-# In[129]:
+# In[24]:
 
 
 # uso un df temporaneo per immagazzinare la join tra df_lenders_normalized e df_loans con solo i campi loan_id
@@ -244,14 +247,14 @@ temp_df.groupby(['country_code', 'disburse_time'])['loan_amount'].sum()/total_le
 temp_df = df_lenders_normalized.merge(df_loans[['loan_id', 'loan_amount']], how='left', on='loan_id')
 
 
-# In[130]:
+# In[25]:
 
 
 # salvo in un df temp 2 il raggruppamento secondo count, per contare il numero totale di ogni loan id
 temp_df2 = temp_df.groupby('loan_id')['lenders'].count().reset_index().rename(columns={"lenders": "tot_num_lender"})
 
 
-# In[133]:
+# In[26]:
 
 
 # faccio ultima merge per avere nel mio data set accanto alla colonna di loan id la colonna dei lenders_y
@@ -259,20 +262,20 @@ temp_df2 = temp_df.groupby('loan_id')['lenders'].count().reset_index().rename(co
 temp_df = temp_df.merge(temp_df2, how='left', on='loan_id')
 
 
-# In[134]:
+# In[27]:
 
 
 temp_df
 
 
-# In[135]:
+# In[28]:
 
 
 # creo nuova colonna per segnare le singole quote per ogni loan_id
 temp_df['single_amount']= temp_df['loan_amount']/temp_df['tot_num_lender']
 
 
-# In[136]:
+# In[29]:
 
 
 # ora raggruppo per ogni lender
@@ -289,14 +292,14 @@ temp_df.groupby('lenders')['single_amount'].sum()
 
 # ***
 
-# In[137]:
+# In[30]:
 
 
 # inizio aggiungendo ai miei dataset anche le informazioni dei lenders_info
-df_lenders_info = pd.read_csv(dire2)
+df_lenders_info = pd.read_csv(dire_df_lenders_info)
 
 
-# In[138]:
+# In[31]:
 
 
 # merge in una temporanea di df_lenders con la normalizzata 
@@ -304,21 +307,21 @@ df_lenders_info = pd.read_csv(dire2)
 temp_df = df_lenders_info[['permanent_name', 'country_code']].merge(df_lenders_normalized, how='left', left_on='permanent_name', right_on='lenders')
 
 
-# In[139]:
+# In[32]:
 
 
 # temp_df2 contiene tutte le righe in cui lenders è presente in loan e non ha lenders == null
 temp_df2 = temp_df.dropna(subset=['lenders'])
 
 
-# In[140]:
+# In[33]:
 
 
 # contiene tutti i borrowed
 temp_df3 = temp_df.loc[temp_df.lenders.isna()]
 
 
-# In[141]:
+# In[34]:
 
 
 # analizzando il temp df 3 mi accorgo che ho qualche country code ma non ho nessun loan amount
@@ -326,95 +329,95 @@ temp_df3 = temp_df.loc[temp_df.lenders.isna()]
 temp_df3
 
 
-# In[142]:
+# In[35]:
 
 
 temp_df2
 
 
-# In[143]:
+# In[36]:
 
 
 # aggiungiamo anche la colonna loan_amount per avere un totale dei lenders/loan/country ( potrebbero esserci dei NULL)
 temp_df2=temp_df2.merge(df_loans[['loan_id', 'loan_amount']],how='left',on='loan_id')
 
 
-# In[144]:
+# In[37]:
 
 
 temp_df2.info()
 
 
-# In[145]:
+# In[38]:
 
 
 # metto i null a 0 di loan amount che sono 4
 temp_df2['loan_amount'] = temp_df2['loan_amount'].fillna(0)
 
 
-# In[146]:
+# In[39]:
 
 
 # inizio controllando quelli con country valore che esiste, perdo quasi 3000000 righe
 df_lenders_loans_country = temp_df2.dropna(subset=['country_code'])
 
 
-# In[147]:
+# In[40]:
 
 
 df_lenders_loans_country = df_lenders_loans_country.groupby('country_code')['loan_amount'].sum()
 
 
-# In[148]:
+# In[41]:
 
 
 total_lent = df_lenders_loans_country.sum()
 
 
-# In[149]:
+# In[42]:
 
 
 df_lenders_loans_country = df_lenders_loans_country.reset_index()
 
 
-# In[150]:
+# In[43]:
 
 
 # cosi facendo ho percentuale del totale per ogni country che è il punto che mi serve
 df_lenders_loans_country['percentage'] = round(df_lenders_loans_country['loan_amount']/total_lent,10)
 
 
-# In[151]:
+# In[44]:
 
 
 df_lenders_loans_country
 
 
-# In[152]:
+# In[45]:
 
 
 temp_df2 = temp_df2.loc[temp_df2.country_code.isna()]
 
 
-# In[153]:
+# In[46]:
 
 
 temp_df2
 
 
-# In[154]:
+# In[47]:
 
 
 total_amount = temp_df2.loan_amount.sum()
 
 
-# In[155]:
+# In[48]:
 
 
 total_amount
 
 
-# In[156]:
+# In[49]:
 
 
 # li moltiplico per la percentuale dei country che conosco lenders e poi li sommo con i precedenti
@@ -423,75 +426,74 @@ x = total_amount * df_lenders_loans_country.percentage
 df_lenders_loans_country['total_lenders_amount_with_no_country'] = x
 
 
-# In[157]:
+# In[50]:
 
 
 df_lenders_loans_country['total_lent'] = df_lenders_loans_country.loan_amount + df_lenders_loans_country.total_lenders_amount_with_no_country
 
 
-# In[158]:
+# In[51]:
 
 
 df_lenders_loans_country
 
 
-# In[159]:
+# In[52]:
 
 
 # no duplicates
-a = df_loans[df_loans.duplicated()]
-print(a)
+df_loans.duplicated()
 
 
-# In[160]:
+# In[53]:
 
 
 temp_df = df_loans.groupby('country_code')['loan_amount'].sum()
 
 
-# In[161]:
+# In[54]:
 
 
 temp_df = temp_df.reset_index()
 
 
-# In[162]:
+# In[55]:
 
 
 temp_df
 
 
-# In[163]:
+# In[56]:
 
 
 del df_lenders_loans_country['total_lenders_amount_with_no_country']
 
 
-# In[164]:
+# In[57]:
 
 
 df_lenders_loans_country = df_lenders_loans_country.merge(temp_df,how='left', on='country_code')
 
 
-# In[165]:
+# In[58]:
 
 
 df_lenders_loans_country
 
 
-# In[166]:
+# In[59]:
 
 
 df_lenders_loans_country.loan_amount_y = df_lenders_loans_country.loan_amount_y.fillna(0)
 
 
-# In[167]:
+# In[60]:
 
 
 df_lenders_loans_country['lent_borrowed'] = df_lenders_loans_country.total_lent - df_lenders_loans_country.loan_amount_y
 
 
-# In[168]:
+# In[61]:
 
 
 df_lenders_loans_country
@@ -503,32 +505,32 @@ df_lenders_loans_country
 
 # ***
 
-# In[169]:
+# In[62]:
 
 
 # importo data set che necessito
-df_country_stat = pd.read_csv(dire3)
+df_country_stat = pd.read_csv(dire_df_country_stat)
 
 
-# In[170]:
+# In[63]:
 
 
 temp_df = df_country_stat.groupby('country_code')['population'].sum()
 
 
-# In[171]:
+# In[64]:
 
 
 temp_df = temp_df.reset_index()
 
 
-# In[172]:
+# In[65]:
 
 
 df_lenders_loans_country = df_lenders_loans_country.merge(temp_df,how='left', on='country_code')
 
 
-# In[173]:
+# In[66]:
 
 
 # se ho popolazione nan metto il primo quartile
@@ -536,13 +538,13 @@ first_quantile = df_lenders_loans_country.population.quantile(0.25)
 df_lenders_loans_country.population = df_lenders_loans_country.population.fillna(first_quantile)
 
 
-# In[174]:
+# In[67]:
 
 
 df_lenders_loans_country
 
 
-# In[175]:
+# In[68]:
 
 
 #calcolo percentuale su i lent-borrowed e la popolazione, successivamente trovo l'idmax per la percentuale che mi dice essere CANADA
@@ -550,14 +552,14 @@ df_lenders_loans_country['percentage'] = round(df_lenders_loans_country['lent_bo
 #temp_df3=temp_df3.reset_index()
 
 
-# In[176]:
+# In[69]:
 
 
 max_ratio = df_lenders_loans_country.loc[df_lenders_loans_country['percentage'].idxmax()]
 min_ratio = df_lenders_loans_country.loc[df_lenders_loans_country['percentage'].idxmin()]
 
 
-# In[177]:
+# In[70]:
 
 
 print(max_ratio,'\n',min_ratio)
@@ -569,70 +571,70 @@ print(max_ratio,'\n',min_ratio)
 
 # ***
 
-# In[178]:
+# In[71]:
 
 
 temp_df2 = df_country_stat.groupby('country_code')[['country_code','population','population_below_poverty_line']].head(175)
 
 
-# In[179]:
+# In[72]:
 
 
 temp_df2['population_below_poverty_line'] = temp_df2['population_below_poverty_line']/100
 
 
-# In[180]:
+# In[73]:
 
 
 temp_df2.country_code = temp_df2.country_code.fillna('NA')
 
 
-# In[181]:
+# In[74]:
 
 
 temp_df2.info()
 
 
-# In[182]:
+# In[75]:
 
 
 mean = temp_df2.population_below_poverty_line.mean()
 temp_df2.population_below_poverty_line = temp_df2.population_below_poverty_line.fillna(mean)
 
 
-# In[183]:
+# In[76]:
 
 
 temp_df2['population_below_poverty_line'] = temp_df2['population'] - (temp_df2['population_below_poverty_line']*temp_df2['population'])
 
 
-# In[184]:
+# In[77]:
 
 
 temp_df2 = temp_df2.merge(df_lenders_loans_country[['country_code', 'lent_borrowed']], left_on='country_code', right_on='country_code')
 
 
-# In[185]:
+# In[78]:
 
 
 mean = temp_df2.lent_borrowed.mean()
 temp_df2.lent_borrowed = temp_df2.lent_borrowed.fillna(mean)
 
 
-# In[186]:
+# In[79]:
 
 
 temp_df2['percentage_poverty'] = round(temp_df2['lent_borrowed']/temp_df2['population_below_poverty_line']*100, 2)
 
 
-# In[187]:
+# In[80]:
 
 
 max_ratio = temp_df2.loc[temp_df2['percentage_poverty'].idxmax()]
 min_ratio = temp_df2.loc[temp_df2['percentage_poverty'].idxmin()]
 
 
-# In[188]:
+# In[81]:
 
 
 print(max_ratio,'\n',min_ratio)
@@ -648,45 +650,45 @@ print(max_ratio,'\n',min_ratio)
 
 # ***
 
-# In[396]:
+# In[82]:
 
 
 temp_df = df_loans[['disburse_time', 'planned_expiration_time', 'loan_amount', 'duration']].dropna()
 
 
-# In[397]:
+# In[83]:
 
 
 temp_df['disburse_time']= temp_df['disburse_time'].dt.tz_localize(None)
 temp_df['planned_expiration_time']= temp_df['planned_expiration_time'].dt.tz_localize(None)
 
 
-# In[398]:
+# In[84]:
 
 
 temp_df['differenza'] = (temp_df.planned_expiration_time.dt.year) - (temp_df.disburse_time.dt.year)
 
 
-# In[399]:
+# In[85]:
 
 
 temp_df.groupby('differenza').size()
 
 
-# In[400]:
+# In[86]:
 
 
 # tengo solo i giorni
 temp_df.duration = temp_df.duration.dt.days
 
 
-# In[401]:
+# In[87]:
 
 
 temp_df.duration
 
 
-# In[402]:
+# In[88]:
 
 
 # aggiungo colonna anno, mese,giorno per comodità
@@ -705,7 +707,7 @@ temp_df.year_disburse_time = temp_df.year_disburse_time.astype('int')
 temp_df.year_planned_expiration_time = temp_df.year_planned_expiration_time.astype('int')
 
 
-# In[478]:
+# In[101]:
 
 
 lista_final = []
@@ -723,7 +725,7 @@ def func_final(df):
     day_sup = lim_sup - df.disburse_time
     day_sup = day_sup.days
     
-    # controllo che non vadano elementi uguali 0 al divisore
+    # controllo che non vadano elementi uguali 0 al divisore caso limite per cui o disburse o planned uguali ai limiti
     if day_inf == 0:
         day_inf = 1
     
@@ -733,8 +735,16 @@ def func_final(df):
     # calcolo quanti giorni per gli anni servono
     year_operation = (365*df.differenza) - 365
     
+    # calcolo gli anni 2012 e 2016 bisestili
+    q = range(df.year_disburse_time, df.year_planned_expiration_time)
+    
+    if 2016 in q == True and year_operation > 0:
+        year_operation = year_operation+1
+    elif 2012 in q == True and year_operation > 0:
+        year_operation = year_operation+1
+    
     flag = df.year_disburse_time
-
+    
     # calcolo quanti giorni per gli anni servono
     divisor = day_inf+day_sup+year_operation
 
@@ -752,8 +762,15 @@ def func_final(df):
             money = safe_division(money_day,divisor)
             result = [money,flag]
             lista_final.append(result)
-            day_sup = 365
+            
+            # bisestile
+            if flag == 2016 or flag == 2012:
+                day_sup = 366
+            else :
+                day_sup = 365
+                
             flag = flag + 1  
+            
             
     # calcolo i restanti giorni
         money_day = df.loan_amount * day_inf
@@ -777,19 +794,19 @@ def safe_division(a, b):
     return a / b
 
 
-# In[479]:
+# In[102]:
 
 
 temp_df.apply(lambda x : func_final(x), axis=1)
 
 
-# In[480]:
+# In[103]:
 
 
 final_df = create_df_year(lista_final)
 
 
-# In[481]:
+# In[104]:
 
 
 final_df.year = final_df.year.astype('int')
